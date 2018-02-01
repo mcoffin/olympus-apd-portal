87e708a4d52563ffba751aa9b570024510fa7c78
Matt Coffin <mcoffin13@gmail.com>
2018-01-31 19:11:43 -0700

    failed attempt at login screen optionality

--- api/ui/app.ts
+++ api/ui/app.ts
@@ -1,6 +1,8 @@
 /// <reference types="node" />
-import { Component, NgModule } from '@angular/core';
+import { Component, NgModule, Input, Output } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { RouterModule, Routes } from '@angular/router';
@@ -10,6 +12,9 @@ import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonToggleModule } from '@angular/material/button-toggle';
 import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { Login } from './login';
+import { OnInit } from '@angular/core';
 
 const style = require('./style.scss');
 
@@ -27,27 +32,42 @@ const routes: Routes = [
     selector: 'apd-portal',
     templateUrl: './apd-portal.html',
 })
-export class ApdPortalComponent {
-    title = "Olympus APD Portal";
+export class ApdPortalComponent implements OnInit {
+    @Input()
+    loggedIn: boolean = false;
+
+    ngOnInit() {
+        if (document.cookie.indexOf("google_access_token=") >= 0) {
+            this.loggedIn = true;
+        } else {
+            this.loggedIn = false;
+        }
+	console.log("loggedIn: " + this.loggedIn);
+    }
 }
 
 @NgModule({
     imports: [
         BrowserModule,
+        FormsModule,
+        HttpModule,
         BrowserAnimationsModule,
         MatToolbarModule,
-	MatMenuModule,
-	MatSidenavModule,
-	MatIconModule,
-	MatButtonToggleModule,
-	MatButtonModule,
-        RouterModule.forRoot(routes, {})
+        MatMenuModule,
+        MatSidenavModule,
+        MatIconModule,
+        MatButtonToggleModule,
+        MatButtonModule,
+        MatCardModule,
+        RouterModule.forRoot(routes, {}),
     ],
     declarations: [
+        Login,
         ApdPortalComponent,
-        PageNotFoundComponent
+        PageNotFoundComponent,
     ],
-    bootstrap: [ ApdPortalComponent ]
+    providers: [],
+    bootstrap: [ ApdPortalComponent ],
 })
 export class AppModule {}
 
